d624b1e9d823bee52662f2c052c6e92fb7da4bce
nelsoncode019 <nelsonher019@gmail.com>
2022-03-01 20:17:04 -0600

    [ADD] Comparing Numbers

---
+++ Comparing Numbers.sh
@@ -0,0 +1,14 @@
+read x
+read y
+
+if (($x < $y)); then
+    echo "X is less than Y"
+fi
+
+if (($x > $y)); then
+    echo "X is greater than Y"
+fi
+
+if (($x == $y)); then
+    echo "X is equal to Y"
+fi
